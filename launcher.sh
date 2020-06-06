aaed738a578c9e375bcf64ef5932673ebd9ebfbe
Alex Gregori Tomás <reproteq@gmail.com>
2020-06-06 23:13:59 +0200

    Create launcher.sh

---
+++ launcher.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# launcher.sh
+# navigate to home directory, then to this directory, then execute python script
+
+cd /
+cd /home/pi/Desktop/Thermographic_Cam_UTI165K_Linux_Python
+sudo python core.py
